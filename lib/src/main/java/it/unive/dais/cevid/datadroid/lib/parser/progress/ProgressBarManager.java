3f51caf0e28c4abed9ab420826db1e1919aedca4
Alvise Spanò <spano.alvise@gmail.com>
2018-01-08 17:13:03 +0100

    Multiple progress bar support for ProgressBarManager

--- lib/src/main/java/it/unive/dais/cevid/datadroid/lib/parser/progress/ProgressBarManager.java
+++ lib/src/main/java/it/unive/dais/cevid/datadroid/lib/parser/progress/ProgressBarManager.java
@@ -1,11 +1,16 @@
 package it.unive.dais.cevid.datadroid.lib.parser.progress;
 
 import android.app.Activity;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.util.Log;
 import android.view.View;
 import android.widget.ProgressBar;
+
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import it.unive.dais.cevid.datadroid.lib.util.Function;
@@ -15,48 +20,101 @@ import it.unive.dais.cevid.datadroid.lib.util.Function;
  */
 public class ProgressBarManager {
 
+    protected static class Descr {
+        @NonNull
+        public final ProgressBar progressBar;
+        @Nullable
+        public Object owner;
+
+        public Descr(@NonNull ProgressBar progressBar, @Nullable Object owner) {
+            this.progressBar = progressBar;
+            this.owner = owner;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("[bar(%s) owner(%s)]", System.identityHashCode(progressBar), System.identityHashCode(owner));
+        }
+    }
+
     private static final String TAG = "ProgressBarManager";
     @NonNull
-    protected final ProgressBar progressBar;
-    @NonNull
-    protected final ConcurrentLinkedQueue<Object> owners = new ConcurrentLinkedQueue<>();
+    protected final ConcurrentLinkedQueue<Descr> q;
     @NonNull
     protected final Activity ctx;
 
-    public ProgressBarManager(@NonNull Activity ctx, @NonNull ProgressBar progressBar) {
-        this.progressBar = progressBar;
+    public ProgressBarManager(@NonNull Activity ctx, @NonNull Collection<ProgressBar> progressBars) {
         this.ctx = ctx;
+        this.q = new ConcurrentLinkedQueue<>();
+        for (ProgressBar p : progressBars)
+            this.q.add(new Descr(p, null));
+    }
+
+    public ProgressBarManager(@NonNull Activity ctx, @NonNull ProgressBar progressBar) {
+        this(ctx, new ProgressBar[] { progressBar });
+    }
+
+
+    public ProgressBarManager(Activity ctx, ProgressBar[] progressBars) {
+        this(ctx, Arrays.asList(progressBars));
     }
 
     @NonNull
     public Handle<ProgressBar> acquire(@NonNull Object owner) {
-        synchronized (owners) {
-            if (owners.isEmpty()) onFirstAcquire();
-            owners.add(owner);
+        synchronized (q) {
+            Descr d = findOwned(owner);
+//            if (owners.isEmpty()) onFirstAcquire();
+//            owners.add(owner);
             return new Handle<ProgressBar>() {
                 @Override
                 public void close() {
-                    synchronized (owners) {
-                        owners.remove(owner);
-                        if (owners.isEmpty()) onLastRelease();
+                    synchronized (q) {
+                        for (Descr d : q) {
+                            if (d.owner == owner) {
+                                Log.d(TAG, String.format("releasing descr %s", d));
+                                d.owner = null;
+                            }
+                        }
                     }
                 }
 
                 @Override
                 @Nullable
                 public <R> R apply(@NonNull Function<ProgressBar, R> f) {
-                    synchronized (owners) {
-                        Object top = owners.peek();
-                        if (top != null && owner == top)
-                            return f.apply(progressBar);
-                        else {
-                            Log.d(TAG, String.format("owner(%s) != myself(%s): apply skipped", owner, this));
-                            return null;
+                    synchronized (q) {
+                        Descr d = findOwned(owner);
+                        if (d != null) {
+                            return f.apply(d.progressBar);
                         }
+                        else return null;
                     }
                 }
             };
         }
+    }
+
+    @Nullable
+    private Descr findOwned(Object owner) {
+        for (Descr d : q) {
+            if (d.owner == owner) {
+                Log.d(TAG, String.format("found descr %s already owned by %s", d, System.identityHashCode(owner)));
+                return d;
+            }
+        }
+        return allocateUnused(owner);
+    }
+
+    @Nullable
+    private Descr allocateUnused(Object owner) {
+        for (Descr d : q) {
+            if (d.owner == null) {
+                d.owner = owner;
+                Log.d(TAG, String.format("allocated descr %s", d));
+                return d;
+            }
+        }
+        Log.d(TAG, String.format("cannot allocate bar now (owner %s is waiting)", System.identityHashCode(owner)));
+        return null;
     }
 
     public void release(@NonNull Handle<ProgressBar> h) {
